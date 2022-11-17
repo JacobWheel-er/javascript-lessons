328729c02292407a9f5df531c1ed6a6015332be5
Jacob Wheeler <115048992+JacobWheel-er@users.noreply.github.com>
2022-11-17 10:00:20 -0500

    Create block-scope.js

---
+++ block-scope.js
@@ -0,0 +1,9 @@
+function logVisibleLightWaves () {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+}
+
+logVisibleLightWaves();
+
+// Will give ReferenceError
+console.log(lightWaves);
